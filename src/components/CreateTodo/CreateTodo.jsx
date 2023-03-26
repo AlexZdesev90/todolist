965691dd54930afc64e5d4cf6eb29ac1c8c6960e
AlexZdesev90 <zdesev-aleksandr@mail.ru>
2023-03-26 16:37:06 +0300

    add create todo, theme, time

---
+++ src/components/CreateTodo/CreateTodo.jsx
@@ -0,0 +1,37 @@
+import {Component} from 'react';
+
+
+export class CreateTodo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            titleValue: '',
+            descriptionValue: '',
+        }
+    }
+
+    onClickAddTodoHandlier(title, description) {
+        // const newTodo = {
+        //         id: new Date(),
+        //         title: title,
+        //         description: description,  
+        //     }
+            this.props.setItems(title, description)
+            // console.log(newTodo)
+    }
+
+    render () {
+        const {titleValue, descriptionValue} = this.state;
+        return (
+            <div>
+                <div>
+                    <label>Title: </label><input type='text' value={titleValue} onChange={(e) => this.setState({titleValue: e.target.value})}/>
+                </div>
+                <div>
+                    <label>Description: </label><input type='text' value={descriptionValue} onChange={(e) => this.setState({descriptionValue: e.target.value})}/>
+                </div>
+                <button onClick={() => this.onClickAddTodoHandlier(titleValue, descriptionValue)}>Add</button>
+            </div>
+        )
+    }
+}
