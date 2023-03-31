c38bdd28a8f69de78a08037168a58d6d016a4995
AlexZdesev90 <zdesev-aleksandr@mail.ru>
2023-03-31 08:58:15 +0300

    add theme and themeswitcher

--- src/components/Theme/ThemeSwitcher.jsx
+++ src/components/Theme/ThemeSwitcher.jsx
@@ -1,18 +1,17 @@
 import {Component} from 'react';
 import classes from '../Theme/Theme.module.css';
 import { HiMoon, HiOutlineSun } from "react-icons/hi";
+import ThemeContext from '../../ThemeContext';
 
 export class ThemeSwitcher extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+
     render () {
-    // const {theme} = this.props
-        return (
-            <div className={classes.theme} {...this.props}>
-                {this.props.theme === 'light' ? <HiMoon/> : <HiOutlineSun/>}
-                {/* <span>{theme}</span> */}
-            </div>
+    const {darkTheme, changeTheme} = this.context;
+        return (<div className={classes.theme} {...this.props} onClick={() => changeTheme()}>
+                    {darkTheme === true ? <HiMoon className={classes.icon}/> : <HiOutlineSun className={classes.icon}/>}
+                </div>
         )
     }
 }
+
+ThemeSwitcher.contextType = ThemeContext;
