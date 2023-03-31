95e1194b96a8c7036a90e58aec589d7e3de1485f
AlexZdesev90 <zdesev-aleksandr@mail.ru>
2023-03-31 08:54:52 +0300

    add time

--- src/components/Time/Time.jsx
+++ src/components/Time/Time.jsx
@@ -4,12 +4,12 @@ import classes from '../Time/Time.module.css';
 export class Time extends Component {
 
     getDay() {
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         const date = new Date();
         return date.getDate() + ' ' + months[date.getMonth()];
     }
+    
     render () {
-
         return (
             <div className={classes.date}>
                 {this.getDay()}
