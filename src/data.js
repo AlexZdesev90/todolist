0f06316bee6179968c34263e9e93c1593d719698
AlexZdesev90 <zdesev-aleksandr@mail.ru>
2023-03-31 09:03:16 +0300

    delete src/data.js

--- src/data.js
+++
@@ -1,9 +0,0 @@
-export const ITEMS = [
-    {id: 1, title: "fsfsfsfsfs1", description: "asdafkalkljdklafhal"},
-    {id: 2, title: "fsfsfsfsfs2", description: "fdsfdfsfkljdklafhal"},
-    {id: 3, title: "fsfsfsfsfs3", description: "ggggggggggggdklafhal"},
-    {id: 4, title: "fsfsfsfsfs4", description: "hhhhhlafhal"},
-    {id: 5, title: "fsfsfsfsfs5", description: "aaaaaahal"},
-    {id: 6, title: "fsfsfsfsfs6", description: "ghrrrrrrrfhal"},
-    {id: 7, title: "fsfsfsfsfs7", description: "qqqqqqqqqqqqqlafhal"},
-]
